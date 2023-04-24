18ae70256910b203847519f491c7403b29d0a76f
moses <shroomce@gmail.com>
2023-04-24 14:52:14 +0500

    clang-formatted

--- src_vadi/main.cpp
+++ src_vadi/main.cpp
@@ -1,5 +1,5 @@
-#include <iostream>
 #include <cstddef>
+#include <iostream>
 
 // ------------------------------ globals & declarations
 #define THREAD_N 4
@@ -12,39 +12,38 @@ void start(size_t threads[THREAD_N]);
 
 // ------------------------------ main
 int main(int argc, char *argv[]) {
-	if (argc == 1) {
-		start(thread_n_arr);
-	} else {
-		start(argv, argc);
-	}
+    if (argc == 1) {
+        start(thread_n_arr);
+    } else {
+        start(argv, argc);
+    }
 }
 
 // ------------------------------ definitions
 bool str_isdigit(char *str) {
-	while (*str) {
-		if (!(*str >= '0' && *str <= '9')) {
-			return false;
-		}
-		++str;
-	}
-	return true;
+    while (*str) {
+        if (!(*str >= '0' && *str <= '9')) {
+            return false;
+        }
+        ++str;
+    }
+    return true;
 }
 
 void start(char **threads, size_t n) {
-	for (size_t i = 1; i < n; ++i) {
-		if (!(str_isdigit(threads[i]))) {
-			std:: cout << "Invalid thread count cmd-argument: " << threads[i] << '\n';
-		} else {
-			// work(atoi(threads[i]));
-			std::cout << "Working with thread count of " << threads[i] << '\n';
-		}
-	}
+    for (size_t i = 1; i < n; ++i) {
+        if (!(str_isdigit(threads[i]))) {
+            std::cout << "Invalid thread count cmd-argument: " << threads[i] << '\n';
+        } else {
+            // work(atoi(threads[i]));
+            std::cout << "Working with thread count of " << threads[i] << '\n';
+        }
+    }
 }
 
 void start(size_t threads[THREAD_N]) {
-	for (size_t i = 0; i < THREAD_N; ++i) {
-		// work(threads[i]);
-		std::cout << "Working with thread count of " << threads[i] << '\n';
-	}
+    for (size_t i = 0; i < THREAD_N; ++i) {
+        // work(threads[i]);
+        std::cout << "Working with thread count of " << threads[i] << '\n';
+    }
 }
-
