a175f8a5c2730ce5644893b2a91a0228e72f4ba1
just born <justborney@gmail.com>
2023-04-24 22:10:14 +0500

    add: example_77

---
+++ example/example_77.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <algorithm>
+#include <future>
+#include <map>
+
+static std::map<char, size_t> histogram(const std::string &s) {
+    std::map<char, size_t> m;
+    for (char c : s) {
+        m[c] += 1;
+    }
+
+    return m;
+}
+
+static std::string sorted(std::string s) {
+    std::sort(s.begin(), s.end());
+
+    return s;
+}
+
+static bool is_vowel(char c) {
+    std::string vowels = "аяуюеёэиы";
+
+    return vowels.end() != std::find(vowels.begin(), vowels.end(), c);
+}
+
+static size_t vowels(const std::string &s) {
+    return std::count_if(s.begin(), s.end(), is_vowel);
+}
+
+int main() {
+    std::string input;
+    std::getline(std::cin, input);
+
+    auto hist = std::async(std::launch::async, histogram, input);
+    auto sorted_str = std::async(std::launch::async, sorted, input);
+    auto vowel_count = std::async(std::launch::async, vowels, input);
+    
+    for (const auto &[c, count] : hist.get()) {
+        std::cout << c << ": " << count << '\n';
+    }
+
+    std::cout << "Sorted string: " << sorted_str.get() << '\n' << "Vowels count: " << vowel_count.get() << '\n';
+}
